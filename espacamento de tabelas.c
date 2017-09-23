dad52e3c4ce5de330b39802714ce1a7defb40207
Leandro da Silva <ld_silva13@hotmail.com>
2017-09-23 17:55:07 -0300

    Adição
    Funcao para utilização em exercícios

---
+++ espacamento de tabelas.c
@@ -0,0 +1,13 @@
+/*criar uma string de tamanho 3 no programa a ser chamo a funcao*/
+
+
+
+void spc(int valor, char espaco[]){   //determina o espacamento da tabela de acordo com o espaco ocupado pelas notas
+
+	if (valor < 10)
+	{
+		strcpy(espaco, "   |");
+	}else{
+		strcpy(espaco, "   |");
+	}
+}
