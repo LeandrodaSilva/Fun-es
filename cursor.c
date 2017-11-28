ff3fd0c95457836f841e408edeb1f8739257a18c
LeandrodaSilva <ld_silva13@hotmail.com>
2017-11-28 01:29:19 -0200

    comment

--- cursor.c
+++ cursor.c
@@ -1,3 +1,4 @@
+/*Desabilita o cursor do console*/
 #include <stdio.h>
 #include <windows.h>
 
@@ -5,7 +6,7 @@
 
 int main(int argc, char const *argv[]) {
   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); //adquire o o console em uso na aplicacao
-  CONSOLE_CURSOR_INFO info; 
+  CONSOLE_CURSOR_INFO info;
   info.dwSize = 10; //define o tamanho do cursor
   info.bVisible = FALSE; //Desabilita o cursor do console
   char nome[50];
